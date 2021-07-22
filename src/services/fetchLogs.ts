dbc39811d0cb6282123de59bfe495909bc663b69
Aditya Vijay <aditya.vijay@redmatter.com>
2021-07-22 23:35:03 +0530

    added further code

--- src/services/fetchLogs.ts
+++ src/services/fetchLogs.ts
@@ -6,6 +6,11 @@ const fetchLogs = async (callIndex: number): Promise<logType[]> => {
     throw new Error('Error fetching logs.');
   }
   const logs = await response.json();
+  /*
+  //playing with promise
+  const logs: any = await fetch('/api/logs.json').then((res) => res.json()).then((res) => new Promise((re) => {
+    setTimeout(() => re(res), 3000)
+  })).then((res) => res);*/
   return logs.slice(0, callIndex);
 }
 
